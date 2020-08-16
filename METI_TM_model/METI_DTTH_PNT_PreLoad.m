eb291d4d644632b4077746c179bf552a49eba9b2
E00026 <E00026@822RCP01.win.iaec.isuzu.co.jp>
2020-08-16 19:11:43 +0900

    <Add> METI_TM_PNT.slx METI_DTTH_PNT.slx METI_TM_CNT.slx METI CVTサーマルモデルのトランスミッションプラント、サーマル、制御モデルの参照モデルとモデルのPreLoadスクリプトを追加

---
+++ METI_TM_model/METI_DTTH_PNT_PreLoad.m
@@ -0,0 +1,5 @@
+% METI_DTTH_PNT model file preload script
+% load Bus object and model parameter
+
+load METI_Thermal_ver01_Jmaab_2018b_Intg_Mref_conversion_data;
+load T_RD_TM;
---
+++ METI_TM_model/METI_TM_PNT_PreLoad.m
@@ -0,0 +1,4 @@
+% METI_TM_PLT model file preload script
+% load Bus object and model parameter
+
+load METI_Thermal_ver01_Jmaab_2018b_Intg_Mref_conversion_data0;
