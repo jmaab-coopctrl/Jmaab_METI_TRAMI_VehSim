e735449b58cf5bcd60c21230f7e4d34094da49a2
nishito <masaaki-nishito@isuzu.co.jp>
2020-08-19 15:01:21 +0900

    <Add> METI_CVT_Slct.m TRAMI_AT_Slct.m トランスミッション プラント、サーマル、制御モデル、ドライバー、Monitorサブシステムを入れ替えるスクリプトを追加

---
+++ METI_TM_model/METI_CVT_Slct.m
@@ -0,0 +1,5 @@
+TM_ModelType = 2;
+
+METI_TM_PNT_BusInfo;
+METI_DTTH_PNT_BusInfo;
+METI_TM_CNT_BusInfo;
---
+++ TRAMI_TM_model/TRAMI_AT_Slct.m
@@ -0,0 +1,5 @@
+TM_ModelType = 3;
+
+TRAMI_TM_PNT_BusInfo;
+TRAMI_DTTH_PNT_BusInfo;
+TRAMI_TM_CNT_BusInfo;
