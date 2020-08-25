df3a09103a05bef16877709856b02604b96c59cf
nishito-isuzu <53589083+nishito-isuzu@users.noreply.github.com>
2020-08-25 17:30:38 +0900

    <Update> METI_CVT_Slct.m, TRAMI_CVT_Slct.m　バスオブジェクトを上書き箇所を無効化

--- METI_TM_model/METI_CVT_Slct.m
+++ METI_TM_model/METI_CVT_Slct.m
@@ -1,8 +1,8 @@
 TM_ModelType = 2;
 
-METI_TM_PNT_BusInfo;
-METI_DTTH_PNT_BusInfo;
-METI_TM_CNT_BusInfo;
+% METI_TM_PNT_BusInfo;
+% METI_DTTH_PNT_BusInfo;
+% METI_TM_CNT_BusInfo;
 
 sampling_time = 0.0025;
 end_time = 1210;
--- TRAMI_TM_model/TRAMI_AT_Slct.m
+++ TRAMI_TM_model/TRAMI_AT_Slct.m
@@ -1,8 +1,8 @@
 TM_ModelType = 3;
 
-TRAMI_TM_PNT_BusInfo;
-TRAMI_DTTH_PNT_BusInfo;
-TRAMI_TM_CNT_BusInfo;
+% TRAMI_TM_PNT_BusInfo;
+% TRAMI_DTTH_PNT_BusInfo;
+% TRAMI_TM_CNT_BusInfo;
 
 sampling_time = 0.001;
 end_time = 1800;
